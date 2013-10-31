041715481e34a6ece8070a0e98259a2361eabe3d
Apprentice <apprentice@dbc-27.local>
2013-10-31 11:41:08 -0700

    Add comment table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131030181450) do
+ActiveRecord::Schema.define(:version => 20131031183817) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "question_id"
+    t.string   "content"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "questions", :force => true do |t|
     t.string   "title"
