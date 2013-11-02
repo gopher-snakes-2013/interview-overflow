01245a100a2bddcddb382ee4891d7d4a0ff10301
Apprentice <apprentice@dbc17.local>
2013-11-01 18:57:06 -0700

    add sad path for errors on ajax comments

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -2,11 +2,17 @@ var Comment = {
 
 	init: function(){
 		$('form#new_comment').on('ajax:success', this.appendComment);
+		$('form#new_comment').on('ajax:error', this.appendErrors);
 	},
 
 	appendComment: function(event, data, status, xhr){
 		$('ul#comment_list').append(data.insightful_comment);
+	},
+
+	appendErrors: function(event, xhr, status, error){
+		$('ul#comment_list').before($.parseJSON(xhr.responseText).error)
 	}
+
 }
 
 $(document).ready(function(){
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,9 @@ class CommentsController < ApplicationController
 		@comment = Comment.new(params[:comment])
 		@comment.question_id = params[:question_id]
 		if @comment.save
-			render :json => {:insightful_comment => render_to_string( :partial => 'comment', :locals => { :comment => @comment})}
+			render json: { insightful_comment: render_to_string( partial: 'comment', locals: { comment: @comment }) }
 		else
-
+			render json: { error: @comment.errors.full_messages.join(", ")}, status: :uprocessable_entity
 		end
 	end
 
