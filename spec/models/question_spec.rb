f266770f8f2c05ef63388e1aa5fba1e7dab075fe
Robert Taraya <robert.taraya@gmail.com>
2013-11-03 01:56:56 -0700

    add home links to views. validate has_many comments association for question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe Question do
-	
+
 	it {should validate_presence_of(:title)}
 	it {should validate_presence_of(:description)}
+  it {should have_many(:comments)}
 
 end
