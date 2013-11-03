a005223f8cd4062d0e80bcd9d5426deae107f124
Robert Taraya <robert.taraya@gmail.com>
2013-11-03 01:20:32 -0800

    add question#create spec test for invalid attributes

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,27 +3,35 @@ require 'spec_helper'
 describe QuestionsController do
   let!(:question) { FactoryGirl.create(:question) }
 
-	context '#index' do
+	describe '#index' do
     it 'visits the index page' do
       get :index
       expect(response).to render_template(:index)
     end
-		it 'should list all questions' do
-			get :index
-			expect(assigns(:questions)).not_to be_nil
-		end
-
 		it 'should have a successful HTTP response code' do
 			get :index
 			response.status.should eq(200)
 		end
+    it 'should list all questions' do
+      get :index
+      expect(assigns(:questions)).not_to be_nil
+    end
   end
 
-  context '#create' do
-    it "creates a question" do
-      expect{
-        post :create, question: {title: question.title, description: question.description }
-        }.to change(Question,:count).by(1)
+  describe '#create' do
+    context 'when given valid attributes' do
+      it "creates a question" do
+        expect{
+          post :create, question: {title: question.title, description: question.description }
+          }.to change(Question,:count).by(1)
+      end
+    end
+    context 'when given invalid attributes' do
+      it 'does not create a question' do
+        expect{
+          post :create, question: {title: question.title}
+        }.to_not change(Question,:count)
+      end
     end
   end
 end
