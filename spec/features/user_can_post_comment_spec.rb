24409fff7ac4cd670e2b7f6be11240788f73b5a8
Apprentice <apprentice@dbc16.local>
2013-11-03 18:39:14 -0800

    i will be back

--- spec/features/user_can_post_comment_spec.rb
+++ spec/features/user_can_post_comment_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe "User vists a question page" do
+describe "User can visit a question page to leave a comment" do
   let!(:question) { create(:question) }
   let!(:comment) { create(:comment) }
-
-  it "can add a comment" do
-    visit question_path(question.id)
-    fill_in "comment[content]", with: comment.content
-    click_on "Post Comment"
-    expect(page).to have_content comment.content
+  describe 'when visting a question page' do
+    it "should post a valid comment" do
+      visit question_path(question.id)
+      fill_in "comment[content]", with: comment.content
+      click_on "Post Comment"
+      expect(page).to have_content comment.content
+    end
   end
-end
