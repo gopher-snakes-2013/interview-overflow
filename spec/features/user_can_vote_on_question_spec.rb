359f6fbf8659780955eddd84e317b24fbc33b359
Apprentice <apprentice@dbc16.local>
2013-11-03 17:30:19 -0800

    will be back soon

--- spec/features/user_can_vote_on_question_spec.rb
+++ spec/features/user_can_vote_on_question_spec.rb
@@ -1,21 +1,20 @@
 require 'spec_helper'
 
-feature 'User can vote on a question' do
+feature 'User can vote up a question' do
 
   before :each do
     visit root_path
-    fill_in('question[title]', with: 'my title')
-    fill_in('question[description]', with: 'my content')
+    fill_in('question[title]', with: 'Do you hate short people?')
+    fill_in('question[description]', with: "Just because I'm tall doesn't mean I hate short people.")
     click_button('Submit Question')
   end
 
-  context 'when on homepage' do
-
-    it 'should see a +1 button' do
+  context 'when visiting homepage with a list of Question posts' do
+    it 'should see a +1 button next to the question' do
       click_button('+1')
     end
 
-    context 'user clicks +1 button' do
+    context 'when user clicks +1 button' do
       it 'should result in a change in the votes field for that question' do
         click_button('+1')
         expect(Question.first.votes).to eq(1)
